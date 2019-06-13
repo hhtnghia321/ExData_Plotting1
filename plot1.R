25313934b1f07330bf8f490879fea4ca65f38327
hhtnghia321 <nghiahht1902@gmail.com>
2019-06-14 01:41:03 +0700

    done

--- plot1.R
+++ plot1.R
@@ -1,28 +1,30 @@
 #download household power consomption data set
-download.file("Electric power consumption", destfile = paste0(getwd(), "/HPC.zip"), method = curl)
-
+if (file.exists("HPC.zip") == TRUE) {
+  download.file("Electric power consumption", destfile = paste0(getwd(), "/HPC.zip"), method = curl)
+}
 #unzip the data set file
-unzip("HPC.zip")
+if (file.exists("household_power_consumption.txt") == TRUE) {
+  unzip("HPC.zip")
+}
 
 #load necessary package
 library(dplyr)
 
 #read the data set file
-data <-  read.table("household_power_consumption.txt", sep = ";", header = T)
+data <-  read.table("household_power_consumption.txt", sep = ";", header = T, colClasses = c("character", "character", rep("numeric",7)), na.strings = "?")
 
 #convert Date and Time
 data$Date <-  as.Date(data$Date, "%d/%m/%Y")
-#data$Time <-- strptime(data$Time, format = "%H:%M:%S")
 
 #subset the data from 2007-02-01 to 2007-02-02
-fildata <- filter(data, Date == as.Date(c("2007-02-01","2007-02-02"), "%Y-%m-%d"))
-
-#convert other variables into numeric type
-fildata[3:9] <- lapply(fildata[3:9], as.character) %>% sapply(as.numeric)
+fildata <- filter(data, !is.na(Global_reactive_power))
+fildata <- filter(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 
 #set the histogram
-hist(fildata$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power" )
+hist(fildata$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 
 #save the plot into png file
-dev.copy(png, "plot1.png")
+dev.copy(png, "plot1.png", width= 480, height = 480)
 dev.off()
+
+
